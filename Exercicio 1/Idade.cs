b1d3e62151e47ff88845e226afb0da5ee093c74c
João Dias <djoao.pedro2012@gmail.com>
2025-02-06 23:39:00 -0300

    Initial commit

---
+++ Exercicio 1/Idade.cs
@@ -0,0 +1,7 @@
+﻿namespace Exercicio_1__Classe__Objeto_e_Atributo_{
+    class Idade {
+        public int A;
+        public int B;
+
+    }
+}
---
+++ Exercicio 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using Exercicio_1__Classe__Objeto_e_Atributo_;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Idade Ia, Ib;
+
+            Ia = new Idade();
+            Ib = new Idade();
+
+            Console.WriteLine("Um nome");
+            string N = Console.ReadLine();
+            Console.WriteLine("Uma idade");
+            Ia.A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Outro nome");
+            string N2 = Console.ReadLine();
+            Console.WriteLine("Outra idade");
+            Ib.B = int.Parse(Console.ReadLine());
+
+            if (Ia.A < Ib.B)
+            {
+                Console.WriteLine($"{N2} E mais velho(a)");
+            }
+            else if (Ia.A > Ib.B)
+            {
+                Console.WriteLine($"{N} E mais velho(a)");
+            }
+            else if (Ia.A == Ib.B)
+            {
+                Console.WriteLine("Ambos possuem a mesma idade");
+            }
+        }
+    }
+}
